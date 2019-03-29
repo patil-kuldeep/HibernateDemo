f34d7b3801e155c12f4ff4ce0cbbe075e54058a1
Test33 User33 <test33user33@gmail.com>
2019-03-29 12:23:20 +0530

    Added some more commands for adding, removing the entities

--- src/main/java/school/client/CommandLine.java
+++ src/main/java/school/client/CommandLine.java
@@ -2,17 +2,28 @@ package school.client;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import school.model.Principal;
+import school.model.School;
 import school.model.Student;
+import school.model.Teacher;
 import school.service.SchoolService;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class CommandLine {
-    static final String ADD_STUDENT = "add";
-    static final String SHOW_STUDENT = "show";
-    static final String REMOVE_STUDENT = "remove";
-    static final List<String> commands = Arrays.asList(SHOW_STUDENT, ADD_STUDENT);
+    static final String ADD_STUDENT = "add_student";
+    static final String SHOW_STUDENT = "show_student";
+    static final String REMOVE_STUDENT = "remove_student";
+    static final String ADD_SCHOOL = "add_school";
+    static final String REMOVE_SCHOOL = "remove school";
+    static final String ADD_TEACHER = "add_teacher";
+    static final String ADD_TEACHER_TO_SCHOOL = "add_t_to_school";
+    static final String ADD_STUDENT_TO_SCHOOL = "add_s_to_school";
+    static final String ADD_PRINCIPAL = "add_principal";
+    static final String ADD_PRINCIPAL_TO_SCHOOL = "add_p_to_school";
+
+    static final List<String> commands = Arrays.asList(ADD_PRINCIPAL_TO_SCHOOL, SHOW_STUDENT, ADD_STUDENT, REMOVE_STUDENT, ADD_TEACHER, ADD_TEACHER_TO_SCHOOL, ADD_STUDENT_TO_SCHOOL, REMOVE_SCHOOL, ADD_PRINCIPAL, ADD_SCHOOL);
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
@@ -32,7 +43,108 @@ public class CommandLine {
             case REMOVE_STUDENT:
                 removeStudent(args, schoolService);
                 break;
+            case ADD_SCHOOL:
+                addSchool(args, schoolService);
+                break;
+            case ADD_TEACHER:
+                addTeacher(args, schoolService);
+                break;
+            case ADD_TEACHER_TO_SCHOOL:
+                addTeacherToSchool(args, schoolService);
+                break;
+            case ADD_STUDENT_TO_SCHOOL:
+                addStudentToSchool(args, schoolService);
+                break;
+            case ADD_PRINCIPAL:
+                addPrincipal(args, schoolService);
+                break;
+            case ADD_PRINCIPAL_TO_SCHOOL:
+                addPrincipalToSchool(args, schoolService);
+                break;
         }
+    }
+
+    private static void addPrincipalToSchool(String[] args, SchoolService schoolService) {
+        if (args.length != 3) {
+            printHelpMessage();
+            return;
+        }
+        try {
+            schoolService.addPrincipalToSchool(Integer.parseInt(args[2]), Integer.parseInt(args[1]));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void addPrincipal(String[] args, SchoolService schoolService) {
+        if (args.length != 6) {
+            printHelpMessage();
+            return;
+        }
+        Principal principal = new Principal();
+        principal.setFirstName(args[1]);
+        principal.setLastName(args[2]);
+        principal.setYearsOfExperience(Integer.parseInt(args[3]));
+        principal.setAge(Integer.parseInt(args[4]));
+        principal.setGender(args[5].charAt(0));
+
+        schoolService.insertPrincipal(principal);
+    }
+
+
+    private static void addStudentToSchool(String[] args, SchoolService schoolService) {
+        if (args.length != 3) {
+            printHelpMessage();
+            return;
+        }
+        try {
+            schoolService.addStudentToSchool(Integer.parseInt(args[2]), Integer.parseInt(args[1]));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void addTeacherToSchool(String[] args, SchoolService schoolService) {
+        if (args.length != 3) {
+            printHelpMessage();
+            return;
+        }
+        try {
+            schoolService.addTeachersToSchool(Arrays.asList(Integer.parseInt(args[1])), Integer.parseInt(args[2]));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void addTeacher(String[] args, SchoolService schoolService) {
+        if (args.length != 5) {
+            printHelpMessage();
+            return;
+        }
+        Teacher teacher = new Teacher();
+        teacher.setFirstName(args[1]);
+        teacher.setLastName(args[2]);
+        teacher.setAge(Integer.parseInt(args[3]));
+        teacher.setGender(args[4].charAt(0));
+
+        schoolService.insertTeacher(teacher);
+    }
+
+    private static void addSchool(String[] args, SchoolService schoolService) {
+        if (args.length != 3) {
+            printHelpMessage();
+            return;
+        }
+        String schoolName = args[1];
+        String phoneNumber = args[2];
+
+        School school = new School();
+        school.setName(schoolName);
+        school.setPhoneNo(phoneNumber);
+
+        schoolService.insertSchool(school);
     }
 
     private static void showStudent(String[] args, SchoolService schoolService) {
@@ -46,8 +158,12 @@ public class CommandLine {
     }
 
     private static void removeStudent(String[] args, SchoolService schoolService) {
-        String rollno = args[1];
-        // remove student
+        if (args.length != 2) {
+            printHelpMessage();
+            return;
+        }
+        String name = args[1];
+        schoolService.deleteStudent(name);
     }
 
     private static void addStudent(String[] args, SchoolService schoolService) {
@@ -59,14 +175,21 @@ public class CommandLine {
         student.setFirstName(name);
         student.setRollNo(roll_no);
         student.setAge(age);
+        student.setGender('M');
 
         schoolService.insertStudent(student);
     }
 
     private static void printHelpMessage() {
         System.out.println("Usage: java -jar <show|add|remove> <parameters>");
-        System.out.println("\t\t show   <id>                       : Show a student");
-        System.out.println("\t\t add    <rollno> <firstName> <age> : Add a student");
-        System.out.println("\t\t remove <id>                       : Remove a student");
+        System.out.println("\t\t show_student   <id>                       : Show a student");
+        System.out.println("\t\t add_student    <rollno> <firstName> <age> : Add a student");
+        System.out.println("\t\t remove_student <firstName>                : Remove a student");
+        System.out.println("\t\t add_school <ame> <phone_number>           : Add a School");
+        System.out.println("\t\t add_teacher <fName> <lName> <age> <gender>: Add a Teacher");
+        System.out.println("\t\t add_teacher_to_school <t_id> <school_id>  : Add a Teacher to School");
+        System.out.println("\t\t add_student_to_school <s_id> <school_id>  : Add a Student to School");
+        System.out.println("\t\t add_principal <fName> <lName> <y_of_exp> <age> <gender>  : Add Principal");
+        System.out.println("\t\t add_principal_to_school <p_id> <school_id> : Add Principal to School");
     }
 }
